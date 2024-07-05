1d3659a63d60497e3a9fcc76ba93683b4af776e6
sergio segovia <sergiosebastian033@gmail.com>
2024-07-05 17:34:13 -0300

    Configuracion de vercel

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import moviesRoutes from './movies/movies.routes.js';
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 app.use(express.json())
